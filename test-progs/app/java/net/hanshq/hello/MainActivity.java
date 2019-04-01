d0f2d44f2a63c8e2e3f87dac9b62d609591a55f2
Peter Collingbourne <pcc@google.com>
2019-04-01 11:44:47 -0700

    add a button for loading the dfm

--- test-progs/app/java/net/hanshq/hello/MainActivity.java
+++ test-progs/app/java/net/hanshq/hello/MainActivity.java
@@ -9,12 +9,6 @@ import android.widget.TextView;
 public class MainActivity extends Activity {
     static {
         System.loadLibrary("loader");
-        loadDFM();
-
-        // This doesn't actually load libhello.so (that was done in loadDFM), it
-        // just registers it with the Java runtime so that JNI calls work
-        // correctly.
-        System.loadLibrary("hello");
     }
 
     public native String getMessage();
@@ -27,7 +21,19 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         final TextView text = (TextView)findViewById(R.id.my_text);
-        text.setText(getMessage());
+        text.setText("Please insert disk 2");
+
+        final Button load = findViewById(R.id.load_dfm);
+        load.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+               loadDFM();
+
+               // This doesn't actually load libhello.so (that was done in loadDFM), it
+               // just registers it with the Java runtime so that JNI calls work
+               // correctly.
+               System.loadLibrary("hello");
+            }
+        });
 
         final Button button = findViewById(R.id.my_button);
         button.setOnClickListener(new View.OnClickListener() {
