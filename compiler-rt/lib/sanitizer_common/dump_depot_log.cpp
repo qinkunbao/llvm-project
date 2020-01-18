445850a6fd9feb31759b35c7063252f4481dbd00
Peter Collingbourne <peter@pcc.me.uk>
2020-01-17 17:26:18 -0800

    dumper

---
+++ compiler-rt/lib/sanitizer_common/dump_depot_log.cpp
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdint.h>
+
+#include <vector>
+#include <memory>
+
+struct StackTraceLog {
+  uintptr_t next;
+  uintptr_t size;
+};
+
+int main(int argc, char **argv) {
+  long addr = 0;
+
+  char proc_name[64];
+  snprintf(proc_name, 64, "/proc/%s/maps", argv[1]);
+  FILE *maps = fopen(proc_name, "r");
+  long begin, end;
+  char desc[512];
+  while (fscanf(maps, "%lx-%lx %512[^\n]\n", &begin, &end, desc) == 3) {
+    if (strstr(desc, "[anon:depot log]")) {
+      addr = begin;
+      break;
+    }
+  }
+  fclose(maps);
+
+  if (!addr) {
+    puts("not found");
+    return 1;
+  }
+
+  snprintf(proc_name, 64, "/proc/%s/mem", argv[1]);
+  int fd = open(proc_name, O_RDONLY);
+  lseek(fd, addr, SEEK_SET);
+  uintptr_t log_addr;
+  read(fd, &log_addr, sizeof(log_addr));
+
+  printf("addr = %lx\n", addr);
+  printf("log_addr = %lx\n", log_addr);
+
+  std::vector<std::unique_ptr<uintptr_t[]>> blobs;
+  StackTraceLog header;
+  while (log_addr) {
+    lseek(fd, log_addr, SEEK_SET);
+    read(fd, &header, sizeof(header));
+    log_addr = header.next;
+
+    auto blob = std::make_unique<uintptr_t[]>(header.size + 1);
+    blob[0] = header.size;
+    read(fd, blob.get() + 1, header.size * sizeof(uintptr_t));
+    blobs.push_back(std::move(blob));
+  }
+  close(fd);
+
+  fd = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0777);
+  for (auto i = blobs.rbegin(); i != blobs.rend(); ++i)
+    write(fd, i->get(), ((*i)[0] + 1) * sizeof(uintptr_t));
+  close(fd);
+}
