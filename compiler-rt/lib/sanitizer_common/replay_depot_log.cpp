0370c3922d99eef0954a94a42bdc0634d8be39b5
Peter Collingbourne <peter@pcc.me.uk>
2020-01-17 17:42:27 -0800

    replay

---
+++ compiler-rt/lib/sanitizer_common/replay_depot_log.cpp
@@ -0,0 +1,17 @@
+#include "sanitizer_common.h"
+#include "sanitizer_file.h"
+#include "sanitizer_stackdepot.h"
+
+using namespace __sanitizer;
+
+int main(int argc, char **argv) {
+  uptr size;
+  uptr *log = (uptr *)MapFileToMemory(argv[1], &size);
+  uptr *log_end = log + (size / sizeof(uptr));
+
+  while (log < log_end) {
+    StackTrace trace(log + 1, log[0]);
+    StackDepotPut(trace);
+    log += log[0] + 1;
+  }
+}
