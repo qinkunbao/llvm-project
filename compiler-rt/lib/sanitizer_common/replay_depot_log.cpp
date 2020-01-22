20f65b5f78d3209dd61ccdebd44120d6f95ffb62
Peter Collingbourne <peter@pcc.me.uk>
2020-01-22 11:47:36 -0800

    rss

--- compiler-rt/lib/sanitizer_common/replay_depot_log.cpp
+++ compiler-rt/lib/sanitizer_common/replay_depot_log.cpp
@@ -3,6 +3,8 @@
 #include "sanitizer_stackdepot.h"
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 using namespace __sanitizer;
 
@@ -11,6 +13,12 @@ int main(int argc, char **argv) {
   uptr *log = (uptr *)MapFileToMemory(argv[1], &size);
   uptr *log_end = log + (size / sizeof(uptr));
 
+#ifndef PERF
+  char cmd[64];
+  snprintf(cmd, 64, "grep RssAnon: /proc/%d/status", getpid());
+  system(cmd);
+#endif
+
   uptr num_inserts = 0;
   while (log < log_end) {
     StackTrace trace(log + 1, log[0]);
@@ -18,6 +26,10 @@ int main(int argc, char **argv) {
     log += log[0] + 1;
     ++num_inserts;
   }
+
+#ifndef PERF
+  system(cmd);
+#endif
 
   uptr num_entries = 0;
   for (uptr i = 0; i != theDepot.kTabSize; ++i) {
