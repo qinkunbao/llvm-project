46bf3b987e30cd86db943ed079c0b721098c4d28
Peter Collingbourne <peter@pcc.me.uk>
2020-02-05 14:44:54 -0800

    cleanup command

--- compiler-rt/lib/scudo/standalone/tools/compute_size_class_config.cpp
+++ compiler-rt/lib/scudo/standalone/tools/compute_size_class_config.cpp
@@ -1,3 +1,4 @@
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -29,26 +30,34 @@ size_t measure_wastage(const std::vector<alloc> &allocs,
   return total_wastage;
 }
 
-std::vector<alloc> read_allocs(FILE *f) {
-  std::vector<alloc> allocs;
+void read_allocs(std::vector<alloc> &allocs, const char *path) {
+  FILE *f = fopen(path, "r");
+  if (!f) {
+    fprintf(stderr, "compute_size_class_config: could not open %s: %s\n", path,
+            strerror(errno));
+    exit(1);
+  }
+
   const char header[] = "<malloc version=\"scudo-1\">\n";
   char buf[sizeof(header) - 1];
   if (fread(buf, 1, sizeof(header) - 1, f) != sizeof(header) - 1 ||
       memcmp(buf, header, sizeof(header) - 1) != 0) {
-    fprintf(stderr, "compute_size_classes: invalid input format\n");
+    fprintf(stderr, "compute_size_class_config: invalid input format\n");
     exit(1);
   }
 
   alloc a;
   while (fscanf(f, "<alloc size=\"%zu\" count=\"%zu\"/>\n", &a.size, &a.count) == 2)
     allocs.push_back(a);
-  return allocs;
+  fclose(f);
 }
 
 size_t log2_floor(size_t x) { return sizeof(long) * 8 - 1 - __builtin_clzl(x); }
 
 void usage() {
-  fprintf(stderr, "usage: compute_size_classes [-p page_size] [-c largest_class] [-h header_size] [-n num_classes] [-b num_bits]\n");
+  fprintf(stderr,
+          "usage: compute_size_class_config [-p page_size] [-c largest_class] "
+          "[-h header_size] [-n num_classes] [-b num_bits] profile...\n");
   exit(1);
 }
 
@@ -59,48 +68,44 @@ int main(int argc, char **argv) {
   size_t num_classes = 32;
   size_t num_bits = 5;
 
+  std::vector<alloc> allocs;
   for (size_t i = 1; i != argc;) {
-    if (i + 1 >= argc)
-      usage();
-    if (strcmp(argv[i], "-p") == 0) {
-      page_size = atoi(argv[i + 1]);
-      i += 2;
+    auto match_arg = [&](size_t &arg, const char *name) {
+      if (strcmp(argv[i], name) == 0) {
+        if (i + 1 != argc) {
+          arg = atoi(argv[i + 1]);
+          i += 2;
+        } else {
+          usage();
+        }
+        return true;
+      }
+      return false;
+    };
+    if (match_arg(page_size, "-p") || match_arg(largest_class, "-c") ||
+        match_arg(header_size, "-h") || match_arg(num_classes, "-n") ||
+        match_arg(num_bits, "-b"))
       continue;
-    }
-    if (strcmp(argv[i], "-c") == 0) {
-      largest_class = atoi(argv[i + 1]);
-      i += 2;
-      continue;
-    }
-    if (strcmp(argv[i], "-h") == 0) {
-      header_size = atoi(argv[i + 1]);
-      i += 2;
-      continue;
-    }
-    if (strcmp(argv[i], "-n") == 0) {
-      num_classes = atoi(argv[i + 1]);
-      i += 2;
-      continue;
-    }
-    if (strcmp(argv[i], "-b") == 0) {
-      num_bits = atoi(argv[i + 1]);
-      i += 2;
-      continue;
-    }
-    usage();
+    read_allocs(allocs, argv[i]);
+    ++i;
   }
 
-  std::vector<alloc> allocs = read_allocs(stdin);
+  if (allocs.empty())
+    usage();
+
   std::vector<size_t> classes;
   classes.push_back(largest_class);
   for (size_t i = 1; i != num_classes; ++i) {
     size_t min_wastage = -1ull;
     size_t min_wastage_class;
     for (size_t new_class = 16; new_class != largest_class; new_class += 16) {
+      // Skip classes with more than num_bits bits, ignoring leading or trailing
+      // zero bits.
       if (__builtin_ctzl(new_class - header_size) +
               __builtin_clzl(new_class - header_size) <
           sizeof(long) * 8 - num_bits)
         continue;
+
       classes.push_back(new_class);
       size_t new_wastage = measure_wastage(allocs, classes, page_size, header_size);
       classes.pop_back();
