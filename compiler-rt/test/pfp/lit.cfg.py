85da2b40172b211b4e1697adb4c564d4c77a2f06
Qinkun Bao <qinkun@google.com>
2025-04-03 19:41:10 +0000

    Implement a simple e2e test for PFP.

---
+++ compiler-rt/test/pfp/lit.cfg.py
@@ -0,0 +1,37 @@
+# -*- Python -*-
+
+import os
+
+from lit.llvm import llvm_config
+from lit.llvm.subst import ToolSubst, FindTool
+
+# Setup config name.
+config.name = "pfp" + config.name_suffix
+
+# Default test suffixes.
+config.suffixes = [".c", ".cpp"]
+
+if config.host_os not in ["Linux"]:
+    config.unsupported = True
+
+# Setup source root.
+config.test_source_root = os.path.dirname(__file__)
+# Setup default compiler flags used with -fsanitize=memory option.
+clang_cflags = [config.target_cflags] + config.debug_info_flags
+clang_cxxflags = config.cxx_mode_flags + clang_cflags
+clang_pfp_tagged_common_cflags = clang_cflags + [
+    "-fexperimental-pointer-field-protection=tagged"
+]
+
+
+clang_pfp_cxxflags = config.cxx_mode_flags + clang_pfp_tagged_common_cflags
+if config.target_arch == 'aarch64':
+    clang_pfp_cxxflags = clang_pfp_cxxflags + ["-fuse-ld=lld --rtlib=compiler-rt --unwindlib=libunwind  -static-libgcc"]
+
+
+def build_invocation(compile_flags):
+    return " " + " ".join([config.clang] + compile_flags) + " "
+
+
+config.substitutions.append(("%clangxx ", build_invocation(clang_cxxflags)))
+config.substitutions.append(("%clangxx_pfp ", build_invocation(clang_pfp_cxxflags)))
